9a6da61f32e4c676a57fe1463df9e30823742180
Patrick Schwaiger <schwaigerpat@gmail.com>
2025-04-22 16:44:04 +0200

    Add Wuestions and Comment section

--- frontend/src/page/Dashboard.jsx
+++ frontend/src/page/Dashboard.jsx
@@ -1,65 +1,66 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faBell, faPlus, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Event from './../component/event.jsx';
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CustomNavbar from './../component/navbar.jsx';
+import AddBtn from "../component/addBtn.jsx";
+import Question from "../component/question.jsx";
 
 
 const Dashboard = () => {
-  const events = [
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:1},
-      {id:2}
-  ];
+    const events = [
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 1},
+        {id: 2}
+    ];
 
-  const getSlidesToShow = (num) => {
-      return Math.min(num, events.length)
-  };
-
-
+    const getSlidesToShow = (num) => {
+        return Math.min(num, events.length)
+    };
 
 
-  const sliderSettings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: getSlidesToShow(5),
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 5000,
-      cssEase: "linear",
-      centerMode: true,
-      arrows: true,
-      responsive: [
-          {
-              breakpoint: 1850, // or less
-              settings: {
-                  slidesToShow: getSlidesToShow(4),
-                  slidesToScroll: 1,
-              },
-          },
-          {
-              breakpoint: 1500, // or less
-              settings: {
-                  slidesToShow: getSlidesToShow(3),
-                  slidesToScroll: 1,
-              },
-          },
+    const sliderSettings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: getSlidesToShow(5),
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        cssEase: "linear",
+        centerMode: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1850, // or less
+                settings: {
+                    slidesToShow: getSlidesToShow(4),
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1500, // or less
+                settings: {
+                    slidesToShow: getSlidesToShow(3),
+                    slidesToScroll: 1,
+                },
+            },
             {
                 breakpoint: 1150, // or less
                 settings: {
@@ -77,37 +78,13 @@ const Dashboard = () => {
                 },
             },
 
-      ]
-  }
+        ]
+    }
 
-  return (
-      <>
-          <nav className="navbar navbar-light bg-primary justify-content-between w-100">
-              <div className="d-flex justify-content-start btn-group ms-2">
-                  <button className="rounded-circle btn btn-light" type="button">
-                      <FontAwesomeIcon icon={faUser} className="text-dark"/>
-                  </button>
-                  <a className="navbar-brand text-white fw-bold ms-2">User Name </a>
-              </div>
-
-              <div className="d-flex justify-content-end btn-group">
-                  <button className="btn me-2" type="button">
-                      <FontAwesomeIcon icon={faBell} className="text-dark fs-4"/>
-                  </button>
-              </div>
-          </nav>
-
-          <div className="dropstart position-absolute bottom-0 end-0" style={{marginRight: '3VW', marginBottom: '3VW'}}>
-              <button type="button" className="rounded-circle btn btn-primary" id="dropdownMenuButton"
-                      data-bs-toggle="dropdown" aria-expanded="false">
-                  <FontAwesomeIcon icon={faPlus} className="text-white p-2 fs-4"/>
-              </button>
-              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li><a className="dropdown-item" href="#">Ask Question</a></li>
-                  <li><a className="dropdown-item" href="#">Create Event</a></li>
-              </ul>
-          </div>
-
+    return (
+        <>
+            <CustomNavbar/>
+            <AddBtn/>
             <div className="m-auto justify-content-center" style={{width: '90vw'}}>
                 <div className="mt-5">
                     <Slider {...sliderSettings}>
@@ -119,12 +96,44 @@ const Dashboard = () => {
                     </Slider>
                 </div>
             </div>
+            <hr className="mx-5 my-3"/>
 
-              <hr className="mx-5 my-3"/>
+            {/*Inner main*/}
+            <div>
+                <Question/>
+                <Question/>
 
+                <div className="d-flex justify-content-center">
+                    <ul className="pagination">
+                            <li className="page-item">
+                                <a className="page-link" href="#">«</a>
+                            </li>
+                            <li className="page-item active">
+                                <a className="page-link" href="#">1</a>
+                            </li>
+                            <li className="page-item">
+                                <a className="page-link" href="#">2</a>
+                            </li>
+                            <li className="page-item">
+                                <a className="page-link" href="#">3</a>
+                            </li>
+                            <li className="page-item">
+                                <a className="page-link" href="#">4</a>
+                            </li>
+                            <li className="page-item">
+                                <a className="page-link" href="#">5</a>
+                            </li>
+                            <li className="page-item">
+                                <a className="page-link" href="#">»</a>
+                            </li>
+                        </ul>
+                </div>
 
-              </>
-              );
-          };
+            </div>
+            {/*/Inner main*/}
 
-          export default Dashboard;
+        </>
+    );
+};
+
+export default Dashboard;
