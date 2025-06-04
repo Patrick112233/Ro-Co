c5bcb1443c9d32fe502a043647847fb057cfdff3
Patrick <schwaigerpat@gmail.com>
2025-06-05 00:23:43 +0200

    add Docker compose

--- restAPI/env/JWTKeyGenerator.sh
+++ restAPI/env/JWTKeyGenerator.sh
@@ -3,12 +3,12 @@ openssl ecparam -genkey -name secp521r1 -noout -out ecdsa-p521-private.pem
 # public key
 openssl ec -in ecdsa-p521-private.pem -pubout -out ecdsa-p521-public.pem
 
-# PCKs(
+# PKCS8
 openssl pkcs8 -topk8 -inform pem -in ecdsa-p521-private.pem -outform pem -nocrypt -out private_pkcs8.pem
 #openssl ec -pubin -inform PEM -in ecdsa-p521-public.pem -pubout -outform PEM -out publickey-pkcs8.pem
 
-# cert
-openssl req -new -key ecdsa-p521-private.pem -out request.csr
-openssl req -x509 -key ecdsa-p521-private.pem -in request.csr -out certificate_x509.pem -days 365
-#openssl req -new -x509 -key ecdsa-p521-private.pem -out ecdsa-p521-cert.crt -days 24854
-
+# cert (non-interactive)
+SUBJ="/C=DE/ST=Bavaria/L=Rosenheim/O=RoCo/OU=Informatics/CN=RestAPI"
+openssl req -new -key ecdsa-p521-private.pem -out request.csr -subj "$SUBJ"
+openssl req -x509 -key ecdsa-p521-private.pem -in request.csr -out certificate_x509.pem -days 365 -subj "$SUBJ"
+#openssl req -new -x509 -key ecdsa-p521-private.pem -out ecdsa-p521-cert.crt -days 24854 -subj "$SUBJ"
--- restAPI/env/TLSCertGenerator.sh
+++ restAPI/env/TLSCertGenerator.sh
@@ -1,8 +1,34 @@
-keytoolPath=${1:-"/usr/lib/jvm/java-21-openjdk-arm64/bin/keytool"}
-$keytoolPath -genkey -alias RoCoTLS -storetype PKCS12 -keyalg RSA -keysize 4096 -keystore RoCoTLS.p12 -validity 3650
-$keytoolPath -export -keystore RoCoTLS.p12 -alias RoCoTLS -file RoCoTLS.crt
+keytoolPath=$(which keytool)
+if [ -z "$keytoolPath" ]; then
+  echo "keytool not found in PATH"
+  exit 1
+fi
 
-cp "$(dirname "$keytoolPath")/RoCoTLS.p12" ./RoCoTLS.p12
-cp "$(dirname "$keytoolPath")/RoCoTLS.crt" ./RoCoTLS.crt
+# Set default values for non-interactive generation, allow override via environment variables
+DNAME="${DNAME:-CN=RoCoTLS, OU=Informatics, O=RoCo, L=Rosenheim, ST=Bavaria, C=DE}"
+STOREPASS="${STOREPASS}"
+KEYPASS="${KEYPASS}"
+
+"$keytoolPath" -genkeypair \
+  -alias RoCoTLS \
+  -storetype PKCS12 \
+  -keyalg RSA \
+  -keysize 4096 \
+  -keystore RoCoTLS.p12 \
+  -validity 3650 \
+  -dname "$DNAME" \
+  -storepass "$STOREPASS" \
+  -keypass "$KEYPASS"
+
+"$keytoolPath" -exportcert \
+  -keystore RoCoTLS.p12 \
+  -alias RoCoTLS \
+  -file RoCoTLS.crt \
+  -storepass "$STOREPASS"
+
+# Files are already in the current directory; no need to copy
+
+#cp RoCoTLS.p12 ./RoCoTLS.p12
+#cp RoCoTLS.crt ./RoCoTLS.crt
 
 #https://www.thomasvitale.com/https-spring-boot-ssl-certificate/
