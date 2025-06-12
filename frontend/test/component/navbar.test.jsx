11a553ac73631ed95d99f00f120422c686a6a89c
Patrick <schwaigerpat@gmail.com>
2025-06-12 19:39:04 +0200

    fixed naming conflict

--- frontend/test/component/navbar.test.jsx
+++ frontend/test/component/navbar.test.jsx
@@ -1,6 +1,6 @@
 
 import { render, screen } from "@testing-library/react";
-import CustomNavbar from "@/component/Navbar.jsx";
+import CustomNavbar from "@/component/navbar.jsx";
 import axios from "@/util/axios.js";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
