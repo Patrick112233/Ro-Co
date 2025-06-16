2e43b07074b036492cfab86d0906d6e3c7bdc2aa
Patrick <schwaigerpat@gmail.com>
2025-06-16 10:32:38 +0200

    e2e Pipline - docker compose

--- playwright.config.ts
+++ playwright.config.ts
@@ -88,7 +88,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-     command: 'docker-compose up',
+     command: 'docker compose up -d',
      url: 'http://localhost:3000',
      reuseExistingServer: !process.env.CI,
    },
